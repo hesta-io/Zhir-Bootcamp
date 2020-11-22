bf883ef4ff6a503c69148d37458fa4ee85c7a548
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-11-23 01:16:15 +0300

    fix fix-1

--- fix-1.sh
+++ fix-1.sh
@@ -4,7 +4,7 @@ find . -wholename './gt/ckb-200/02_Sarchia_Abdullah/02_Sarchia_Abdullah*.lstmf' 
 find . -wholename './gt/ckb-200/03_Sarchia_Abdulrahman_1/03_Sarchia_Abdulrahman_1*.lstmf' > data/all-03_Sarchia_Abdulrahman_1-ckb-200-lstmf
 find . -wholename './gt/ckb-200/06_Sarchia_Ahmad/06_Sarchia_Ahmad*.lstmf' > data/all-06_Sarchia_Ahmad-ckb-200-lstmf
 find . -wholename './gt/ckb-200/07_Sarchia_Akre/07_Sarchia_Akre*.lstmf' > data/all-07_Sarchia_Akre-ckb-200-lstmf
-find . -wholename './gt/ckb-200/08_Sarchia_Al Arabiya TV_1/08_Sarchia_Al Arabiya TV_1*.lstmf' > data/all-08_Sarchia_Al Arabiya TV_1-ckb-200-lstmf
+find . -wholename './gt/ckb-200/08_Sarchia_Al_Arabiya_TV_1/08_Sarchia_Al_Arabiya_TV_1*.lstmf' > data/all-08_Sarchia_Al_Arabiya_TV_1-ckb-200-lstmf
 find . -wholename './gt/ckb-200/10_Sarchia_Ali/10_Sarchia_Ali*.lstmf' > data/all-10_Sarchia_Ali-ckb-200-lstmf
 find . -wholename './gt/ckb-200/11_Sarchia_Amad/11_Sarchia_Amad*.lstmf' > data/all-11_Sarchia_Amad-ckb-200-lstmf
 find . -wholename './gt/ckb-200/12_Sarchia_Ameer/12_Sarchia_Ameer*.lstmf' > data/all-12_Sarchia_Ameer-ckb-200-lstmf
@@ -18,7 +18,7 @@ find . -wholename './gt/ckb/02_Sarchia_Abdullah/02_Sarchia_Abdullah*.lstmf' > da
 find . -wholename './gt/ckb/03_Sarchia_Abdulrahman_1/03_Sarchia_Abdulrahman_1*.lstmf' > data/all-03_Sarchia_Abdulrahman_1-ckb-lstmf
 find . -wholename './gt/ckb/06_Sarchia_Ahmad/06_Sarchia_Ahmad*.lstmf' > data/all-06_Sarchia_Ahmad-ckb-lstmf
 find . -wholename './gt/ckb/07_Sarchia_Akre/07_Sarchia_Akre*.lstmf' > data/all-07_Sarchia_Akre-ckb-lstmf
-find . -wholename './gt/ckb/08_Sarchia_Al Arabiya TV_1/08_Sarchia_Al Arabiya TV_1*.lstmf' > data/all-08_Sarchia_Al Arabiya TV_1-ckb-lstmf
+find . -wholename './gt/ckb/08_Sarchia_Al_Arabiya_TV_1/08_Sarchia_Al_Arabiya_TV_1*.lstmf' > data/all-08_Sarchia_Al_Arabiya_TV_1-ckb-lstmf
 find . -wholename './gt/ckb/10_Sarchia_Ali/10_Sarchia_Ali*.lstmf' > data/all-10_Sarchia_Ali-ckb-lstmf
 find . -wholename './gt/ckb/11_Sarchia_Amad/11_Sarchia_Amad*.lstmf' > data/all-11_Sarchia_Amad-ckb-lstmf
 find . -wholename './gt/ckb/12_Sarchia_Ameer/12_Sarchia_Ameer*.lstmf' > data/all-12_Sarchia_Ameer-ckb-lstmf
