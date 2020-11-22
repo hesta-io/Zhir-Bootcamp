b67c9a5b2896a5d79cfa119b299ea663b12894a3
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-11-23 00:32:36 +0300

    write fix scripts

---
+++ fix-5.sh
@@ -0,0 +1,28 @@
+find . -wholename './gt/ckb-200/61_Sarchia_Mariwan/61_Sarchia_Mariwan*.lstmf' > data/all-61_Sarchia_Mariwan-ckb-200-lstmf
+find . -wholename './gt/ckb-200/62_Sarchia_Mohsin/62_Sarchia_Mohsin*.lstmf' > data/all-62_Sarchia_Mohsin-ckb-200-lstmf
+find . -wholename './gt/ckb-200/63_Sarchia_Muhammad/63_Sarchia_Muhammad*.lstmf' > data/all-63_Sarchia_Muhammad-ckb-200-lstmf
+find . -wholename './gt/ckb-200/64_Sarchia_Muslim/64_Sarchia_Muslim*.lstmf' > data/all-64_Sarchia_Muslim-ckb-200-lstmf
+find . -wholename './gt/ckb-200/65_Sarchia_Nayab/65_Sarchia_Nayab*.lstmf' > data/all-65_Sarchia_Nayab-ckb-200-lstmf
+find . -wholename './gt/ckb-200/66_Sarchia_Niga/66_Sarchia_Niga*.lstmf' > data/all-66_Sarchia_Niga-ckb-200-lstmf
+find . -wholename './gt/ckb-200/67_Sarchia_Nokia/67_Sarchia_Nokia*.lstmf' > data/all-67_Sarchia_Nokia-ckb-200-lstmf
+find . -wholename './gt/ckb-200/68_Sarchia_Nursi/68_Sarchia_Nursi*.lstmf' > data/all-68_Sarchia_Nursi-ckb-200-lstmf
+find . -wholename './gt/ckb-200/69_Sarchia_Omar/69_Sarchia_Omar*.lstmf' > data/all-69_Sarchia_Omar-ckb-200-lstmf
+find . -wholename './gt/ckb-200/70_Sarchia_Partwk/70_Sarchia_Partwk*.lstmf' > data/all-70_Sarchia_Partwk-ckb-200-lstmf
+find . -wholename './gt/ckb-200/71_Sarchia_Pirmam/71_Sarchia_Pirmam*.lstmf' > data/all-71_Sarchia_Pirmam-ckb-200-lstmf
+find . -wholename './gt/ckb-200/72_Sarchia_Qaisy/72_Sarchia_Qaisy*.lstmf' > data/all-72_Sarchia_Qaisy-ckb-200-lstmf
+find . -wholename './gt/ckb-200/73_Sarchia_Qamishlo/73_Sarchia_Qamishlo*.lstmf' > data/all-73_Sarchia_Qamishlo-ckb-200-lstmf
+
+find . -wholename './gt/ckb/61_Sarchia_Mariwan/61_Sarchia_Mariwan*.lstmf' > data/all-61_Sarchia_Mariwan-ckb-lstmf
+find . -wholename './gt/ckb/62_Sarchia_Mohsin/62_Sarchia_Mohsin*.lstmf' > data/all-62_Sarchia_Mohsin-ckb-lstmf
+find . -wholename './gt/ckb/63_Sarchia_Muhammad/63_Sarchia_Muhammad*.lstmf' > data/all-63_Sarchia_Muhammad-ckb-lstmf
+find . -wholename './gt/ckb/64_Sarchia_Muslim/64_Sarchia_Muslim*.lstmf' > data/all-64_Sarchia_Muslim-ckb-lstmf
+find . -wholename './gt/ckb/65_Sarchia_Nayab/65_Sarchia_Nayab*.lstmf' > data/all-65_Sarchia_Nayab-ckb-lstmf
+find . -wholename './gt/ckb/66_Sarchia_Niga/66_Sarchia_Niga*.lstmf' > data/all-66_Sarchia_Niga-ckb-lstmf
+find . -wholename './gt/ckb/67_Sarchia_Nokia/67_Sarchia_Nokia*.lstmf' > data/all-67_Sarchia_Nokia-ckb-lstmf
+find . -wholename './gt/ckb/68_Sarchia_Nursi/68_Sarchia_Nursi*.lstmf' > data/all-68_Sarchia_Nursi-ckb-lstmf
+find . -wholename './gt/ckb/69_Sarchia_Omar/69_Sarchia_Omar*.lstmf' > data/all-69_Sarchia_Omar-ckb-lstmf
+find . -wholename './gt/ckb/70_Sarchia_Partwk/70_Sarchia_Partwk*.lstmf' > data/all-70_Sarchia_Partwk-ckb-lstmf
+find . -wholename './gt/ckb/71_Sarchia_Pirmam/71_Sarchia_Pirmam*.lstmf' > data/all-71_Sarchia_Pirmam-ckb-lstmf
+find . -wholename './gt/ckb/72_Sarchia_Qaisy/72_Sarchia_Qaisy*.lstmf' > data/all-72_Sarchia_Qaisy-ckb-lstmf
+find . -wholename './gt/ckb/73_Sarchia_Qamishlo/73_Sarchia_Qamishlo*.lstmf' > data/all-73_Sarchia_Qamishlo-ckb-lstmf
+
