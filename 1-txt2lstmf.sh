25d28c93c0613acc23629e3a74b13f154949fe70
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-11-22 07:02:56 +0300

    each font in its own directory

--- 1-txt2lstmf.sh
+++ 1-txt2lstmf.sh
@@ -27,6 +27,9 @@ TESSDATA_PATH=$SCRIPTPATH/tessdata
 echo 'setting TESSDATA_PREFIX to ' $TESSDATA_PATH
 export TESSDATA_PREFIX=$TESSDATA_PATH
 
+mkdir -p gt
+mkdir -p data
+
 font_counter=1
 while IFS= read -r fontname
      do
@@ -35,34 +38,35 @@ while IFS= read -r fontname
 		mkdir -p "${font_dir}"
         cp ${traininginput} ${fonttext}
         for cnt in $(seq 1 $linecount) ; do
-
+			mkdir -p "$prefix"/"${fontname// /_}"/
+			mkdir -p $prefix-200/"${fontname// /_}"/
             remainder=$(( line_counter % 100 ))
             if [ "$remainder" -eq 0 ]; then
                 head -1 ${fonttext} > ${linetext}
                 sed -i  "1,1  d"  ${fonttext}
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}.300.$cnt.exp0" 
-                cp "$linetext" "$prefix"/"${fontname// /_}.300.$cnt.exp0".gt.txt
-                tesseract "$prefix"/"${fontname// /_}.300.$cnt.exp0".tif "$prefix"/"${fontname// /_}.300.$cnt.exp0" -l ara --psm 13 --dpi 300 lstm.train  
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}.200.$cnt.exp0" 
-                cp "$linetext" $prefix-200/"${fontname// /_}.200.$cnt.exp0".gt.txt
-                tesseract $prefix-200/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train  
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" 
+                cp "$linetext" "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".gt.txt
+                tesseract "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".tif "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" -l ara --psm 13 --dpi 300 lstm.train  
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" 
+                cp "$linetext" $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".gt.txt
+                tesseract $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train  
 
                 echo "========== LINES PROCESSED:" $line_counter "of font #" $font_counter "=========="
             else
                 head -1 ${fonttext} > ${linetext}
                 sed -i  "1,1  d"  ${fonttext}
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}.300.$cnt.exp0" 2>/dev/null
-                cp "$linetext" "$prefix"/"${fontname// /_}.300.$cnt.exp0".gt.txt
-                tesseract "$prefix"/"${fontname// /_}.300.$cnt.exp0".tif "$prefix"/"${fontname// /_}.300.$cnt.exp0" -l ara --psm 13 --dpi 300 lstm.train  2>/dev/null
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}.200.$cnt.exp0" 2>/dev/null
-                cp "$linetext" $prefix-200/"${fontname// /_}.200.$cnt.exp0".gt.txt
-                tesseract $prefix-200/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train 2>/dev/null
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" 2>/dev/null
+                cp "$linetext" "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".gt.txt
+                tesseract "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".tif "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" -l ara --psm 13 --dpi 300 lstm.train  2>/dev/null
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" 2>/dev/null
+                cp "$linetext" $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".gt.txt
+                tesseract $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train 2>/dev/null
             fi
 
             line_counter=$(( line_counter + 1 ))
          done
-        ls -1  $prefix/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-lstmf
-        ls -1  $prefix-200/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-200-lstmf
+        ls -1  $prefix/"${fontname// /_}"/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-lstmf
+        ls -1  $prefix-200/${fontname// /_}/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-200-lstmf
         echo "Done with ${fontname// /_}"
         font_counter=$(( font_counter + 1 ))
      done < "$fontlist"
