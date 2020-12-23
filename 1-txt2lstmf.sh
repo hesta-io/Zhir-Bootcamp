b771197ca4c78c3fe717046dc0dcb455e1a9cf61
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-12-23 22:41:49 +0300

    fix gt script and import new corpus

--- 1-txt2lstmf.sh
+++ 1-txt2lstmf.sh
@@ -51,22 +51,28 @@ while IFS= read -r fontname
                 cp "$linetext" $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".gt.txt
                 tesseract $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train  
 
-                echo "========== LINES PROCESSED:" $line_counter "of font #" $font_counter "=========="
+                echo "========== LINES PROCESSED:" $line_counter "of font #" $font_counter " @ " $(date -u) "=========="
             else
                 head -1 ${fonttext} > ${linetext}
                 sed -i  "1,1  d"  ${fonttext}
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" 2>/dev/null
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --rotate_image=true --underline_start_prob=0.1 --distort_image=false --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"   --outputbase="$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" 2>/dev/null
                 cp "$linetext" "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".gt.txt
                 tesseract "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0".tif "$prefix"/"${fontname// /_}"/"${fontname// /_}.300.$cnt.exp0" -l ara --psm 13 --dpi 300 lstm.train  2>/dev/null
-                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" 2>/dev/null
+                text2image --fonts_dir="$unicodefontdir" --text="${linetext}" --rotate_image=true --underline_start_prob=0.1 --distort_image=false --strip_unrenderable_words=false --xsize=2500 --ysize=300  --leading=32 --margin=12 --exposure=0  --font="$fontname"  --resolution=200  --outputbase=$prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" 2>/dev/null
                 cp "$linetext" $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".gt.txt
                 tesseract $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0".tif $prefix-200/"${fontname// /_}"/"${fontname// /_}.200.$cnt.exp0" -l ara --psm 13 --dpi 200 lstm.train 2>/dev/null
             fi
 
             line_counter=$(( line_counter + 1 ))
          done
-        find -maxdepth 1 $prefix/"${fontname// /_}"/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-lstmf
-        find -maxdepth 1 $prefix-200/${fontname// /_}/*${fontname// /_}.*.lstmf > data/all-${fontname// /_}-$MODEL-200-lstmf
+
+        eval "find $SCRIPTPATH -wholename '$SCRIPTPATH/$prefix/${fontname// /_}/${fontname// /_}*.lstmf' > data/all-${fontname// /_}-$MODEL-lstmf"
+        eval "find $SCRIPTPATH -wholename '$SCRIPTPATH/$prefix-200/${fontname// /_}/${fontname// /_}*.lstmf' > data/all-${fontname// /_}-$MODEL-200-lstmf"
+        
+        # for some weird reason, find only works when run by eval :/
+        #find $SCRIPTPATH -wholename '$SCRIPTPATH/$prefix/${fontname// /_}/${fontname// /_}*.lstmf' # > data/all-${fontname// /_}-$MODEL-lstmf
+        #find $SCRIPTPATH -wholename '$SCRIPTPATH/$prefix-200/${fontname// /_}/${fontname// /_}*.lstmf' # > data/all-${fontname// /_}-$MODEL-200-lstmf
+        
         echo "Done with ${fontname// /_}"
         font_counter=$(( font_counter + 1 ))
      done < "$fontlist"
